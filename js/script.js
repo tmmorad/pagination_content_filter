291ad05002d835dd8a81a620b2cf43bff399ddc8
Tamer <32287010+tmmorad@users.noreply.github.com>
2018-09-25 00:24:40 -0700

    wroking with no pagination on search

--- js/script.js
+++ js/script.js
@@ -6,25 +6,26 @@ FSJS project 2 - List Filter and Pagination
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const $allStudents = document.querySelectorAll('li.student-item');
 const studentCount = $allStudents.length;
-let $pageNumbers = Math.ceil((studentCount) / 10);
+let $theNumber = $('li.student-item:visible');
+let $pageNumbers = Math.ceil((studentCount) / 10);//use to be studentCount
 let currentPage = 0;
-let searchResults;
+let searchResults = true;
 
 const $hideStudents = () => $($allStudents).hide(); // function hides all the students
 
 //displays 10 students only from the list, based on the current pagination page
-function $displayStudents(){
+function $displayStudents(topRange){
   let $range = (currentPage + 1) *10;//sets the end point of the slice
   currentPage *= 10; //sets the start point of the slice
-  if (currentPage > studentCount) {
-    $range = studentCount;
+  if (currentPage > topRange) {
+    $range = topRange;
   }
   $hideStudents(); //hides all the students
   //displays only 10 students between the startpoint(currentPage) and the endpoint($range)
   $($allStudents).slice(currentPage, $range).show();
 }
 
-$displayStudents();
+$displayStudents(studentCount);
 
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
@@ -37,7 +38,7 @@ $displayStudents();
 
 function pagination(){
   $('.page').append(`<div class="pagination"><ul></ul></div>`);
-  if (searchResults === 0) {
+  if (searchResults === false) {
     alert("No results match your search! Append this to the HTML")
   } else {
     for (let i = 0; i < $pageNumbers; i+=1) {
@@ -60,19 +61,28 @@ $('div.page-header').append(
 //Event listner for search field
 $('.student-search').on('click keyup', function(){
   let $entered = $('input').val();
-  let $found = 0;
-  $('button').on('click', function(){
+  //on submit searchs for students names that match entered user query
+
+
+  if($entered ==="" || $entered === false){
+    $displayStudents(studentCount);
+  } else {
     $hideStudents();
     $('li.student-item h3').each(function (index, element){
       let $student = $(this).text();
       if($student.includes($entered)){
         $(this).parent().parent().show();
       }
-    });
-
-  });
-  //console.log($('input').val());
-});
+    });//end of .each() loop
+    // if($theNumber.length === 0){
+    //   searchResults = false;
+    //   pagination();
+    // } else {
+    //   searchResults = true;
+    //   pagination();
+    // }
+  }//end of if $entered ESLE statement
+});//Event End
 
 //Event listener on 'click' that sets the active page and returns the page number value
 $('.pagination').on("click","a",function (){
@@ -80,7 +90,7 @@ $('.pagination').on("click","a",function (){
   $(this).addClass("active");
   currentPage = parseInt($(this).attr("value"), 10);
   console.log(currentPage);
-  $displayStudents();
+  $displayStudents(studentCount);//change to visible Students
 });
 //ADDEvent listner  to parent .pagination ul that on click moves class+active to the target
 //and sets the currentPage value to be the number clicked
